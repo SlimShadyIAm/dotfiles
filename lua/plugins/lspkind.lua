f05d709f236494efdfced00243257f2f87098828
Aamir Farooq <aamir@farooq.xyz>
2023-06-07 02:16:36 +0200

    Lspkind for completions

---
+++ lua/plugins/lspkind.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "onsails/lspkind.nvim",
+    config = function()
+      local lspkind = require("lspkind")
+      lspkind.init({
+        mode = 'symbol',
+        symbol_map = {
+          Text = "󰉿",
+          Method = "󰆧",
+          Function = "󰊕",
+          Constructor = "",
+          Field = "󰜢",
+          Variable = "󰀫",
+          Class = "󰠱",
+          Interface = "",
+          Module = "",
+          Property = "󰜢",
+          Unit = "󰑭",
+          Value = "󰎠",
+          Enum = "",
+          Keyword = "󰌋",
+          Snippet = "",
+          Color = "󰏘",
+          File = "󰈙",
+          Reference = "󰈇",
+          Folder = "󰉋",
+          EnumMember = "",
+          Constant = "󰏿",
+          Struct = "󰙅",
+          Event = "",
+          Operator = "󰆕",
+          TypeParameter = "",
+        },
+      })
+    end,
+  },
+}
