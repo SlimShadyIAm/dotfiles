91887e76e2471b99e3fcbe0eb1fef381c2bc8fc7
Aamir Farooq <aamir@farooq.xyz>
2023-06-09 16:20:03 +0200

    Fix to copilot in cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -103,7 +103,7 @@ return {
           { name = "luasnip",  group_index = 2 },
           { name = "buffer",   group_index = 2 },
           { name = "path",     group_index = 2 },
-          { name = "copilot",  group_index = 1 },
+          { name = "copilot",  group_index = 2 },
         },
       })
     end,
