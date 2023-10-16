39d05d8f4be7156d2f2ab64533799f8e121ddbaa
SlimShadyIAm <aamir@farooq.xyz>
2023-10-16 12:57:50 +0300

    Add image preview

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -54,6 +54,19 @@ return {
 					width = 30,
 				},
 				filesystem = {
+					window = {
+						mappings = {
+							["p"] = "image_wezterm", -- " or another map
+						},
+					},
+					commands = {
+						image_wezterm = function(state)
+							local node = state.tree:get_node()
+							if node.type == "file" then
+								require("image_preview").PreviewImage(node.path)
+							end
+						end,
+					},
 					follow_current_file = {
 						enabled = true,
 					},
--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -75,4 +75,11 @@ return {
 			})
 		end,
 	},
+	{
+		"https://github.com/adelarsq/image_preview.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("image_preview").setup()
+		end,
+	},
 }
