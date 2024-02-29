c7fe12e9c1c6b5659a7fc8a24aef8d49abdb5439
SlimShadyIAm <aamir@farooq.xyz>
2024-02-29 14:59:02 +0100

    Fixies

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -29,7 +29,7 @@ end
 config.font = wezterm.font("JetBrains Mono", { weight = "Regular" })
 config.font_size = 13.0
 config.line_height = 1.1
-config.harfbuzz_features = {"zero" , "ss01", "cv05"}
+config.harfbuzz_features =  {'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'calt', 'dlig'}
 
 -- disable alt+enter
 config.keys = {
